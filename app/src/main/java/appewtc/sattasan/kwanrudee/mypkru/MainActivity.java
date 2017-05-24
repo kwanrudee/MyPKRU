f548712df75260573bc0c9ead6abeecd34a97e8a
Kwan <kwan14791@gmail.com>
2017-05-24 10:36:56 +0700

    Finish 24 May

--- app/src/main/java/appewtc/sattasan/kwanrudee/mypkru/MainActivity.java
+++ app/src/main/java/appewtc/sattasan/kwanrudee/mypkru/MainActivity.java
@@ -2,14 +2,57 @@ package appewtc.sattasan.kwanrudee.mypkru;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // Explicit ประกาศตัวแปร
+    private EditText userEditText, passwordEditText;
+    private TextView textView;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initial View
+        initalView();
+
+        // controller
+        controller();
+
+    }  // main Method นี้คือคลาสเม็ดดอต
+
+    private void controller() {
+        textView.setOnClickListener(this);
+        button.setOnClickListener(this);
+    }
+
+    private void initalView() {
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtpassword);
+        textView = (TextView) findViewById(R.id.txtNewRegister);
+        button = (Button) findViewById(R.id.btnLogin);
 
     }
-}
+
+    @Override
+    public void onClick(View v) {
+
+       //For TextView
+        if (v == textView) {
+        }
+
+        //for Button
+        if (v == button) {
+        }
+
+
+    }
+} // Main Class นี้คือคลาสหลัก
+
