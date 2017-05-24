da04e86533fe72b603f67b23a1a9efee5f5c8f03
Kwan <kwan14791@gmail.com>
2017-05-24 14:17:19 +0700

    Finish 24 May3

--- app/src/main/java/appewtc/sattasan/kwanrudee/mypkru/NewRegister2.java
+++ app/src/main/java/appewtc/sattasan/kwanrudee/mypkru/NewRegister2.java
@@ -2,12 +2,36 @@ package appewtc.sattasan.kwanrudee.mypkru;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class NewRegister2 extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, userEditText, passwordEditText;
+    private ImageView backImageView, humanImageView, cameraImageView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_register2);
+
+        //Initial View
+        initialView();
+
+    }  // main Method
+
+    private void initialView() {
+        nameEditText = (EditText) findViewById(R.id.edtName);
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtpassword);
+        backImageView = (ImageView) findViewById(R.id.btnBack);
+        humanImageView= (ImageView) findViewById(R.id.imvHumen);
+        cameraImageView = (ImageView) findViewById(R.id.imvCamera);
+        button = (Button) findViewById(R.id.btnRegister);
+
     }
+    // main Class
 }
