6e650b5110e01607e7a5f7e4a61705e10447e9c7
Kwan <kwan14791@gmail.com>
2017-05-24 15:33:55 +0700

    Finish 24 May4

--- app/src/main/java/appewtc/sattasan/kwanrudee/mypkru/NewRegister2.java
+++ app/src/main/java/appewtc/sattasan/kwanrudee/mypkru/NewRegister2.java
@@ -1,12 +1,15 @@
 package appewtc.sattasan.kwanrudee.mypkru;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
-public class NewRegister2 extends AppCompatActivity {
+public class NewRegister2 extends AppCompatActivity implements View.OnClickListener {
 
     //Explicit
     private EditText nameEditText, userEditText, passwordEditText;
@@ -21,7 +24,28 @@ public class NewRegister2 extends AppCompatActivity {
         //Initial View
         initialView();
 
+        //Controller
+        controller();
+
     }  // main Method
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+            //For Human
+        if ((requestCode == 0)&& (resultCode == RESULT_OK)) {
+            Log.d("24MayV1", "Human OK");
+        }
+    }
+
+    private void controller() {
+        backImageView.setOnClickListener(this);
+        humanImageView.setOnClickListener(this);
+        cameraImageView.setOnClickListener(this);
+        button.setOnClickListener(this);
+
+    }
 
     private void initialView() {
         nameEditText = (EditText) findViewById(R.id.edtName);
@@ -31,6 +55,23 @@ public class NewRegister2 extends AppCompatActivity {
         humanImageView= (ImageView) findViewById(R.id.imvHumen);
         cameraImageView = (ImageView) findViewById(R.id.imvCamera);
         button = (Button) findViewById(R.id.btnRegister);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        //For Back
+        if (v == backImageView) {
+            finish();
+        }
+
+          //For Human
+        if (v == humanImageView) {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("image/*");
+            startActivityForResult(Intent.createChooser(intent, "Please Choose App for Choose Image"), 0);
+        }
 
     }
     // main Class
