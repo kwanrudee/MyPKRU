d18d0ea59f4f7bdd6ae12cf247ea66aa4a5d73a9
Kwan <kwan14791@gmail.com>
2017-05-25 17:07:46 +0700

    25.5.60

--- app/src/main/java/appewtc/sattasan/kwanrudee/mypkru/MainActivity.java
+++ app/src/main/java/appewtc/sattasan/kwanrudee/mypkru/MainActivity.java
@@ -1,20 +1,33 @@
 package appewtc.sattasan.kwanrudee.mypkru;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import appewtc.sattasan.kwanrudee.mypkru.GetAllData;
+import appewtc.sattasan.kwanrudee.mypkru.MyAlert;
+import appewtc.sattasan.kwanrudee.mypkru.NewRegister2;
+import appewtc.sattasan.kwanrudee.mypkru.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    // Explicit ประกาศตัวแปร
+    //Explicit
     private EditText userEditText, passwordEditText;
     private TextView textView;
     private Button button;
     private String userString, passwordString;
+    private String[] loginStrings;
+    private String[] columnStrings = new String[]{"id", "Name", "User", "Password", "Image"};
+    private boolean aBoolean = true;
 
 
     @Override
@@ -22,39 +35,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initial View
-        initalView();
+        //Initial View
+        initialView();
 
-        // controller
+        //Controller
         controller();
 
-    }  // main Method นี้คือคลาสเม็ดดอต
+
+    }   // Main Method
 
     private void controller() {
         textView.setOnClickListener(this);
         button.setOnClickListener(this);
     }
 
-    private void initalView() {
+    private void initialView() {
         userEditText = (EditText) findViewById(R.id.edtUser);
         passwordEditText = (EditText) findViewById(R.id.edtpassword);
         textView = (TextView) findViewById(R.id.txtNewRegister);
         button = (Button) findViewById(R.id.btnLogin);
-
     }
 
     @Override
-    public void onClick(View v) {
+    public void onClick(View view) {
 
-       //For TextView
-        if (v == textView) {
-             //Intent to NewRegister
-            Intent intent = new Intent(MainActivity.this,NewRegister2.class);
+        //For TextView
+        if (view == textView) {
+            //Intent to NewRegister
+            Intent intent = new Intent(MainActivity.this, NewRegister2.class);
             startActivity(intent);
         }
 
-        //for Button
-        if (v == button) {
+        //For Button
+        if (view == button) {
+
             userString = userEditText.getText().toString().trim();
             passwordString = passwordEditText.getText().toString().trim();
 
@@ -63,11 +77,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 MyAlert myAlert = new MyAlert(this);
                 myAlert.myDialog(getResources().getString(R.string.titleHaveSpace),
                         getResources().getString(R.string.messageHaveSpace));
-
             } else {
-                // No Space
+                //No Space
                 checkUserAnPass();
             }
+
         }
 
 
@@ -75,8 +89,60 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void checkUserAnPass() {
 
-        String urlPHP = "http://swiftcodingthai.com/pkru/GetUserKwan.php";
+        String urlPHP = "http://swiftcodingthai.com/pkru/GetUserMaster.php";
+
+        try {
+
+            GetAllData getAllData = new GetAllData(this);
+            getAllData.execute(urlPHP);
+
+            String strJSON = getAllData.get();
+            Log.d("25MayV1", "JSON ==> " + strJSON);
+
+            JSONArray jsonArray = new JSONArray(strJSON);
+            loginStrings = new String[columnStrings.length];
+
+            for (int i=0;i<jsonArray.length();i++) {
+
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                if (userString.equals(jsonObject.getString(columnStrings[2]))) {
+                    aBoolean = false;
+
+                    for (int i1=0;i1<columnStrings.length;i1++) {
+                        loginStrings[i1] = jsonObject.getString(columnStrings[i1]);
+                        Log.d("25MayV2", "loginString(" + i1 + ") ==> " + loginStrings[i1]);
+                    }   // for2
+
+                }   // if
+
+            }   // for1
+
+            //check User And Password
+            if (aBoolean) {
+                //user False
+                MyAlert myAlert = new MyAlert(this);
+                myAlert.myDialog(getResources().getString(R.string.titleUserFalse),
+                        getResources().getString(R.string.messageUserFalse));
+            } else if (passwordString.equals(loginStrings[3])) {
+                //Password True
+                Toast.makeText(MainActivity.this,
+                        "Welcome " + loginStrings[1], Toast.LENGTH_SHORT).show();
+
+            } else {
+                //Password False
+                MyAlert myAlert = new MyAlert(this);
+                myAlert.myDialog(getResources().getString(R.string.titlePassword),
+                getResources().getString(R.string.messagePassword));
+            }
+
+
+
+
+        } catch (Exception e) {
+            Log.d("25MayV1", "e checkUser ==> " + e.toString());
+        }
+
+
 
     }
-} // Main Class นี้คือคลาสหลัก
-
+}   // Main Class นี่คือ คลาสหลัก
